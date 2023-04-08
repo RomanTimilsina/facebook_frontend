2c5bca8d81780600526518ed6090d1ca35e5ff0b
Roman <romantimilsina@gmail.com>
2023-04-08 08:09:22 +0545

    login_component_made

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,32 @@
+import './login.scss'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <div className="card">
+        <div className="left">
+          <h1>
+            Hello World
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum accusamus nobis quam mollitia, saepe esse et quas quasi fugiat unde reprehenderit, ab sunt distinctio qui. Hic maiores nulla tenetur reiciendis?
+          </p>
+          <span>Don't have an account?</span>
+          <button>
+            Register
+          </button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder='Username'/>
+            <input type="password" placeholder='password' />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,11 @@
+import './register.scss'
+
+const Register = () => {
+  return (
+    <div className='register'>
+      register
+    </div>
+  )
+}
+
+export default Register
